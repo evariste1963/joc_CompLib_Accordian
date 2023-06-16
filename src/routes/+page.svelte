a18dbe7d9f3928d8a675971f3592b1c98dac322c
evariste1963 <evariste@hotmail.co.uk>
2023-06-16 22:08:41 +0100

    setup @ home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,7 @@
 
 	export let data;
 	console.log(data);
-	//create items in a json file somewhere//
-	/*const items = [
-		{ title: 'item 1', content: 'content 1' },
-		{ title: 'item 2', content: 'content 2' },
-		{ title: 'item 3', content: 'content 3' },
-		{ title: 'item 4', content: 'content 4' }
-	];*/
+	
 </script>
 
 <h1>It all starts here!</h1>
