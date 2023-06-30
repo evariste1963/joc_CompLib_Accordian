517c961414af9fc28e7b19341cb8993855390bef
evariste1963 <evariste@hotmail.co.uk>
2023-06-30 08:21:15 +0100

    hover linear-gradient added

--- src/lib/components/accordion/accordion-item.svelte
+++ src/lib/components/accordion/accordion-item.svelte
@@ -79,10 +79,12 @@
 		transition: background-color 0.3s ease;
 	}
 	.accordion-toggle:hover {
-		background-color: var(--accordion-hover, hsl(220 20% 20%));
+		/*background-color: var(--accordion-hover, hsl(220 20% 20%));*/
+		background: var( --accordion-hover-linear-gradient, linear-gradient(rgba(246, 247, 248, 0.3) 5%, rgba(50, 20, 182, 0.3) 100%))
 	}
 
 	.accordion-content {
+		font-size: 1.25rem;
 		padding: var(--accordion-content-padding, 1rem);
 		color:var(--accordion-content-color, rgb(240, 225, 212))
 	}
--- src/lib/components/accordion/accordion.svelte
+++ src/lib/components/accordion/accordion.svelte
@@ -21,6 +21,6 @@
 		color: var(--accordion-color, hsl(220 10% 98%));
 		background-color: var(--accordion-background, hsl(220 10% 16%));
 		border-radius: var(--accordion-radius, 12px);
-		box-shadow: var(--accordion-shadow, 0px 2px 25px hsl(210, 14%, 97%), 0px 4px 30px rgb(20, 25, 182));
+		box-shadow: var(--accordion-shadow, 0px 2px 25px rgb(246, 247, 248), 0px 4px 30px rgb(20, 25, 182));
 	}
 </style>
