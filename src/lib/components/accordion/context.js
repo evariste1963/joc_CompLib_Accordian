25cd83e578f95a7973dbd99f1fa92f14219ed597
evariste1963 <evariste@hotmail.co.uk>
2023-07-01 07:20:20 +0100

    ...and colapse again --> context.js

--- src/lib/components/accordion/context.js
+++ src/lib/components/accordion/context.js
@@ -3,12 +3,12 @@ import { setContext, getContext } from 'svelte'
 
 export function setAccordionOptions({ colapse }) {
 	const activeComponentId = writable(null)
-	setContext('collapse', colapse)
+	setContext('colapse', colapse)
 	setContext('active', activeComponentId)
 }
 
 export function getAccordionOptions() {
-	const colapse = getContext('collapse')
+	const colapse = getContext('colapse')
 	const activeComponentId = getContext('active')
 	return { colapse, activeComponentId }
 }
